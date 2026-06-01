e0fff89445658dd7d3cc995c1b799fbc48a7d8b8
shimaa <shimaa.yousry82@gmail.com>
2026-06-01 12:09:20 +0300

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,32 @@
+#------------------------------
+## Escape Sequences Characters
+## \b => Back Space
+# \newline => Escape New Line + \
+# \\ = >Escape Back Slash
+# \' = >Escape Single Quote
+#\" = >Escape Double Quote"
+#\n=> Line Feed
+#\r => Carriage Return
+#\t => Tab
+## \xhh => Character Hex Value
+##------------------------------
+print("Hello, World\b")
+print("hello world")
+print("hello\b world")
+print("hello \
+ilove\
+python")
+# Escape Back Slash
+print ("hello i love back slash \\")
+# Escape Single Quote
+print ('hello i love single quote \'test \'')
+print("hello i love double quote \"test \"")
+#\n=> Line Feed
+
+print("hello world \n new line")
+
+print("123456\rabcd")  # Carriage Return
+
+print("hello\tworld")  # Tab
+
+print("\x53\x59")
